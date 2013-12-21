9d4bc99600a3f2d1c7370520c460abf58e260643
Fred Zirdung <fred@hackreactor.com>
2013-12-21 15:46:55 -0800

    Update to latest version

--- src/underbar.js
+++ src/underbar.js
@@ -1,8 +1,14 @@
 /*jshint eqnull:true, expr:true*/
 
-var _ = { };
+var _ = {};
 
 (function() {
+
+  // Returns whatever value is passed as the argument. This function doesn't
+  // seem very useful, but remember it--if a function needs to provide an
+  // iterator when the user does not pass one in, this will be handy.
+  _.identity = function(val) {
+  };
 
   /**
    * COLLECTIONS
@@ -11,11 +17,22 @@ var _ = { };
    * In this section, we'll have a look at functions that operate on collections
    * of values; in JavaScript, a 'collection' is something that can contain a
    * number of values--either an array or an object.
+   *
+   *
+   * IMPORTANT NOTE!
+   * ===========
+   *
+   * The .first function is implemented for you, to help guide you toward success
+   * in your work on the following functions. Whenever you see a portion of the
+   * assignment pre-completed, be sure to read and understanding it fully before
+   * you proceed. Skipping this step will lead to considerably more difficulty
+   * implementing the sections you are responsible for.
    */
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
@@ -25,6 +42,9 @@ var _ = { };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
+  //
+  // Note: _.each does not have a return value, but rather simply runs the
+  // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
   };
 
@@ -34,15 +54,24 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+    var result = -1;
+
+    _.each(array, function(item, index) {
+      if (item === target && result === -1) {
+        result = index;
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that pass a truth test.
-  _.filter = function(collection, iterator) {
+  _.filter = function(collection, test) {
   };
 
   // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, iterator) {
-    // TIP: see if you can re-use _.select() here, without simply
+  _.reject = function(collection, test) {
+    // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
 
@@ -52,7 +81,7 @@ var _ = { };
 
 
   // Return the results of applying an iterator to each element.
-  _.map = function(array, iterator) {
+  _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
@@ -67,17 +96,18 @@ var _ = { };
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
-  _.pluck = function(array, propertyName) {
+  _.pluck = function(collection, key) {
     // TIP: map is really handy when you want to transform an array of
     // values into a new array of values. _.pluck() is solved for you
     // as an example of this.
-    return _.map(array, function(value){
-      return value[propertyName];
+    return _.map(collection, function(item){
+      return item[key];
     });
   };
 
   // Calls the method named by methodName on each value in the list.
-  _.invoke = function(list, methodName, args) {
+  // Note: you will nead to learn a bit about .apply to complete this.
+  _.invoke = function(collection, functionOrKey, args) {
   };
 
   // Reduces an array or object to a single value by repetitively calling
@@ -85,15 +115,15 @@ var _ = { };
   // the return value of the previous iterator call.
   //
   // You can pass in an initialValue that is passed to the first iterator
-  // call. Defaults to 0.
+  // call. If initialValue is not explicitly passed in, it should default to the
+  // first element in the collection.
   //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //
-  _.reduce = function(collection, iterator, initialValue) {
+  _.reduce = function(collection, iterator, accumulator) {
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -101,7 +131,7 @@ var _ = { };
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function(wasFound, item) {
-      if(wasFound) {
+      if (wasFound) {
         return true;
       }
       return item === target;
@@ -164,10 +194,11 @@ var _ = { };
     // time it's called.
     var alreadyCalled = false;
     var result;
+
     // TIP: We'll return a new function that delegates to the old one, but only
     // if it hasn't been called before.
-    return function(){
-      if(!alreadyCalled){
+    return function() {
+      if (!alreadyCalled) {
         // TIP: .apply(this, arguments) is the standard way to pass on all of the
         // infromation from one function call to another.
         result = func.apply(this, arguments);
@@ -181,7 +212,7 @@ var _ = { };
   // Memoize an expensive function by storing its results. You may assume
   // that the function takes only one argument and that it is a primitive.
   //
-  // Memoize should return a function that when called, will check if it has
+  // _.memoize should return a function that when called, will check if it has
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
@@ -202,7 +233,11 @@ var _ = { };
    * ==============================
    */
 
-  // Shuffle an array.
+  // Randomizes the order of an array's contents.
+  //
+  // TIP: This function's test suite will ask that you not modify the original
+  // input array. For a tip on how to make a copy of an array, see:
+  // http://mdn.io/Array.prototype.slice
   _.shuffle = function(array) {
   };
 
